b2ebcd80beb4cfa46b9982c319d4072e19c2d820
Pukkok <krystalgggg@gmail.com>
2024-11-11 01:54:32 +0900

    tsx 적용 테스트, python 라우터 작동 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import { useState } from 'react';
 import useFetch from './hook/useFetch';
+import './style.css'
 
 const App = () => {
 
-  const [result, setResult] = useState<string[] | string | null>(null)
+  const [result, setResult] = useState<string | null>(null)
 
   const helloAction = async () => {
     const data = await useFetch('/')
     setResult(data)
   }
+  
+  const postTestAction = async () => {
+    const data = await useFetch('/post-test', 'POST', '안녕하세요', 'imsi-token-data')
+    console.log(data)
+  }
 
   const getTop5 = async () => {
     const data = await useFetch('/user-info?option=top_5')
     const newResult = data.map((item : Record<string, number>, idx : number) => {
-      const texts = []
+      let text = ''
       for(let genre in item){
-        const text = `${idx + 1}위 : ${genre}는 ${item[genre]}명이 선호합니다.`
-        texts.push(text)
+        text = `${idx + 1}위 : ${genre}는 ${item[genre]}명이 선호합니다.`
       }
-      return texts
+      return text
     })
     setResult(newResult)
   }
@@ -33,11 +38,11 @@ const App = () => {
     setResult(text)
   }
 
-  
   return (
     <div className="App">
       <h1>파이썬 데이터 결과 보기</h1>
       <button type='button' onClick={helloAction}>안녕 버튼</button>
+      <button type='button' onClick={postTestAction}>테스트 버튼</button>
       <button type='button' onClick={getTop5}>20대 선호 장르 top5</button>
       <button type='button' onClick={getAllList}>20대 선호 장르 전체</button>
       <hr/>
@@ -45,12 +50,17 @@ const App = () => {
         {typeof result === 'string' && <p>{result}</p>}
 
         {Array.isArray(result) &&
-        result.map((item : string) => {
-          return <div key={item}>{item}</div>
-        })}
+         result.map((item : string) => {
+           return <p key={item}>{item}</p>
+         })}
       </div>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
